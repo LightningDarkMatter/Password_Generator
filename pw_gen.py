39883d51c6b63db27ed97a24c6863b5651d2764f
LightningDarkMatter <70206474+LightningDarkMatter@users.noreply.github.com>
2022-06-28 16:37:15 +0100

    Create pw_gen.py

---
+++ pw_gen.py
@@ -0,0 +1,31 @@
+import random
+
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+symbols = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '_', '+', '=', '{', '}', '[', ']', '|', ':', ';', '"', '\'', '<', '>', ',', '.', '?', '/']
+
+print("Welcome to the random password generator.\n")
+nr_letters = int(input("How many letters do you want in your password?\n"))
+nr_numbers = int(input("How many numbers do you want in your password?\n"))
+nr_symbols = int(input("How many symbols do you want in your password?\n"))
+
+password = []
+
+for i in range(nr_letters):
+    letter = random.choice(letters)
+    password.append(letter)
+for i in range(nr_numbers):
+    number = random.choice(numbers)
+    password.append(number)
+for i in range(nr_symbols):
+    symbol = random.choice(symbols)
+    password.append(symbol)
+
+random.shuffle(password)
+
+output = ""
+
+for i in password:
+    output += str(i)
+
+print("Your password is: ", output)
